55a636fdda7d7cadcdc19432de2f44d2af49d7aa
gold-leo <168613134+gold-leo@users.noreply.github.com>
2024-12-12 23:27:24 -0600

    Experiments and testing

--- src/main/java/edu/grinnell/csc207/experiments/PathExperiment.java
+++ src/main/java/edu/grinnell/csc207/experiments/PathExperiment.java
@@ -28,26 +28,49 @@ public class PathExperiment {
     g.addVertex("c");
     g.addVertex("d");
     g.addVertex("e");
-    g.addVertex("f");
-    g.addVertex("g");
+    // g.addVertex("f");
+    // g.addVertex("g");
 
-    g.addEdge("a", "b", 0);
-    g.addEdge("b", "c", 0);
-    g.addEdge("c", "d", 0);
-    g.addEdge("d", "e", 0);
-    g.addEdge("e", "g", 0);
-    g.addEdge("e", "a", 0);
-    g.addEdge("c", "g", 0);
-    g.addEdge("g", "e", 0);
+    g.addEdge("a", "b", 2);
+    g.addEdge("a", "c", 1);
+    g.addEdge("c", "d", 5);
+    g.addEdge("d", "e", 1);
+    g.addEdge("b", "e", 4);
 
-    pen.println(g.path("a", "b"));
-    pen.println(g.path("a", "c"));
-    pen.println(g.path("a", "d"));
-    pen.println(g.path("a", "e"));
-    pen.println(g.path("a", "f"));
-    pen.println(g.path("a", "g"));
-    pen.println(g.path("a", "a"));
-    pen.println("Done");
+    int source = 0; // a
+    int sink = 4; // e
+    Integer[] path = g.shortestPath(source, sink);
+    int i = sink;
+    pen.println();
+    do {
+      pen.print(i + " <-- ");
+      i = path[i];
+    } while (i != source);
+    pen.print(i);
+
+    pen.println();
+    // for (int i = 0; i < path.length; i++) {
+    //   pen.print(path[i] + " ");
+    // }
+    // pen.println();
+
+    
+    // g.addEdge("d", "e", 0);
+    // g.addEdge("e", "g", 0);
+    // g.addEdge("e", "a", 0);
+    // g.addEdge("c", "g", 0);
+    // g.addEdge("g", "e", 0);
+
+    // pen.println(g.path("a", "b"));
+    // pen.println(g.path("a", "c"));
+    // pen.println(g.path("a", "d"));
+    // pen.println(g.path("a", "e"));
+    // pen.println(g.path("a", "f"));
+    // pen.println(g.path("a", "g"));
+    // pen.println(g.path("a", "a"));
+    // pen.println("Done");
+
+    
   } // main(String[])
 
 } // PathExperiment
